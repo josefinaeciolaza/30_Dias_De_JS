99017af430361855cce9d519cf548604d0ea51be
josefinaeciolaza <josefinaecio@gmail.com>
2023-09-29 12:14:45 -0300

    día 10

--- 10_AllowOneFunctionCall.js
+++ 10_AllowOneFunctionCall.js
@@ -3,3 +3,17 @@
 // The first time the returned function is called, it should return the same result as fn.
 // Every subsequent time it is called, it should return undefined.
  
+var once = function(fn) {
+    
+    let notCall = false;
+    let results; 
+	return function(...args){
+        if(!notCall){
+            results = fn(...args)
+            notCall = true;
+            return results
+        }else{
+            return undefined
+        }
+    }
+};
